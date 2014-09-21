c96deaa2769321247049aa79bc798230e42f0754
Deepak Somashekhara <dsoma@yahoo-inc.com>
2014-09-21 13:54:37 -0700

    Adding required pods for the dependent libs

---
+++ yelpClient/YelpClient.h
@@ -0,0 +1,14 @@
+//
+//  YelpClient.h
+//  yelpClient
+//
+//  Created by Deepak Somashekhara on 9/21/14.
+//  Copyright (c) 2014 Deepak Somashekhara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BDBOAuth1RequestOperationManager.h"
+
+@interface YelpClient : BDBOAuth1RequestOperationManager
+
+@end
---
+++ yelpClient/YelpClient.m
@@ -0,0 +1,13 @@
+//
+//  YelpClient.m
+//  yelpClient
+//
+//  Created by Deepak Somashekhara on 9/21/14.
+//  Copyright (c) 2014 Deepak Somashekhara. All rights reserved.
+//
+
+#import "YelpClient.h"
+
+@implementation YelpClient
+
+@end
